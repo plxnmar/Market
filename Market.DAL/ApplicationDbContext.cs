29d3ca400ad1987d47e75b964ee3bb21b73ed77f
Mira0Kit <kitomura@bk.ru>
2022-10-14 17:00:54 +0300

    Добавлен уровень данных - DAL и контекст данных ApplicationDbContext в качестве сервиса

---
+++ Market.DAL/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Market.Domain.Entity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Market.DAL
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
--- Market/Program.cs
+++ Market/Program.cs
@@ -1,7 +1,21 @@
+using Market.DAL;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+
+
+// получаем строку подключения из файла конфигурации
+string connection = builder.Configuration.GetConnectionString("DefaultConnection");
+
+
+// добавляем контекст ApplicationContext в качестве сервиса в приложение
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connection));
+//services.AddControllersWithViews();
 
 var app = builder.Build();
 
