861611e9b00e04f14b7eed17d3be668c05ba38f3
Mira0Kit <kitomura@bk.ru>
2022-10-15 14:33:20 +0300

    nullable значения для name и description

--- Market.Domain/Entity/Product.cs
+++ Market.Domain/Entity/Product.cs
@@ -10,8 +10,8 @@ namespace Market.Domain.Entity
     public class Product
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
         public decimal Price { get; set; }
         public TypeProduct TypeProduct { get; set; }
     }
