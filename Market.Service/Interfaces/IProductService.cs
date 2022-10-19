c6d9086edcd2aa64e4ebeacf57104b1a098b49f6
Mira0Kit <kitomura@bk.ru>
2022-10-19 14:34:53 +0300

    методы сервиса продукта добавлены в его интерфейс

--- Market.Service/Interfaces/IProductService.cs
+++ Market.Service/Interfaces/IProductService.cs
@@ -1,5 +1,7 @@
-﻿using Market.Domain.Entity;
+﻿using Market.DAL.Repositories;
+using Market.Domain.Entity;
 using Market.Domain.Response;
+using Market.Domain.ViewModels.Product;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,5 +13,8 @@ namespace Market.Service.Interfaces
     public interface IProductService
     {
         Task<IBaseResponse<IEnumerable<Product>>> GetProducts();
+        Task<IBaseResponse<Product>> GetProduct(int id);
+        Task<IBaseResponse<bool>> DeleteProduct(int id);
+        Task<IBaseResponse<bool>> AddProduct(ProductViewModel productViewModel);
     }
 }
